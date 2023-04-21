8da6febd8b93db0936ce6069be8fcc392e008e49
JunDonghyuk <84345881+jundonghyuk@users.noreply.github.com>
2023-04-21 13:51:56 +0900

    [Gold II] Title: K번째 수, Time: 252 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        long l = 1;
+        long r = K;
+
+        while (l <= r) {
+            int cnt = 0;
+            long m = (l + r) / 2;
+            for (int i = 1; i <= N; i++) {
+                cnt += Math.min(m / i, N);
+            }
+
+            if (cnt < K) {  // 중복되는 데이터까지 쳤는데, 그럼에도 카운트가 더 적다.
+                l = m + 1;
+            } else {
+
+                r = m - 1;
+            }
+        }
+        System.out.println(l);
+    }
+}
