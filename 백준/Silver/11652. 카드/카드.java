60164efed1d7f7c9f6bf58f231c43f3536d63431
JunDonghyuk <84345881+jundonghyuk@users.noreply.github.com>
2023-07-04 21:13:10 +0900

    [Silver IV] Title: 카드, Time: 476 ms, Memory: 33344 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.java
@@ -0,0 +1,49 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        List<Long> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr.add(Long.parseLong(st.nextToken()));
+        }
+        Collections.sort(arr);
+        long num = arr.get(0);
+        int cnt = 1;
+
+        long tempNum = arr.get(0);
+        int tempCnt = 1;
+        for (int i = 0; i < n - 1; i++) {
+//            tempNum = arr.get(i);
+            if (tempNum != arr.get(i + 1)) {
+                if (cnt < tempCnt) {
+                    cnt = tempCnt;
+                    num = tempNum;
+                    tempCnt = 1;
+                } else {
+                    tempCnt = 1;
+                }
+                tempNum = arr.get(i + 1);
+            } else {
+                tempCnt++;
+            }
+        }
+
+        if (tempCnt > cnt) {
+            num = tempNum;
+        }
+        System.out.println(num);
+    }
+}
