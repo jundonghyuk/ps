3d4aa8c67ec4188654108eec1a02160e3bb7a15c
JunDonghyuk <84345881+jundonghyuk@users.noreply.github.com>
2023-10-10 21:45:39 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 132 ms, Memory: 14780 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -1,31 +1,67 @@
 
+
+import java.util.*;
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
 
+    static int[] dp2 = new int[10001];
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        long[][] dp = new long[10001][4];
 
-        int T = Integer.parseInt(st.nextToken());
-        int[] dp = new int[10001];
-        dp[0] = 1;
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 3;
-        dp[4] = 4;
-        dp[5] = 5;
-        for(int i = 6; i <= 10000; i++) {
-            dp[i] = dp[i-1] + dp[i-2] - dp[i-3] + dp[i-3] - dp[i-4] - (dp[i-5] - dp[i-6]);
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][1] = 1;
         }
-        while (T-- > 0) {
-            int n = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
-            System.out.println(dp[n]);
+//        dp[]
+        dp[2][2] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+        dp[4][2] = 2;
+        dp[4][3] = 1;
+
+        for (int i = 5; i < dp.length; i++) {
+            dp[i][2] = i / 2;
+            int j = i;
+            dp[i][3] = dp[i-3][1] + dp[i-3][2] + dp[i-3][3];
+//            while (j >= 3) {
+//                j -= 3;
+//                dp[i][3] += dp[j][2] + 1;
+//            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n][1] + dp[n][2] + dp[n][3]);
+            if(i != t-1) {
+                sb.append('\n');
+            }
+        }
+        he();
+        for(int i = 1; i <= 10000; i++) {
+            if(dp2[i] != dp[i][1] + dp[i][2] + dp[i][3]) {
+                System.out.println("hello" + i);
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    public static void he() {
+        dp2[0] = 1;
+        dp2[1] = 1;
+        dp2[2] = 2;
+        dp2[3] = 3;
+        dp2[4] = 4;
+        dp2[5] = 5;
+        for(int i = 6; i <= 10000; i++) {
+            dp2[i] = dp2[i-1] + dp2[i-2] - dp2[i-3] + dp2[i-3] - dp2[i-4] - (dp2[i-5] - dp2[i-6]);
         }
     }
+
 }
