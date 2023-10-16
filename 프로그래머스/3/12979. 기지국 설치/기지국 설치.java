c976fb834357270f6f02f9d6be00284beb9812b9
JunDonghyuk <84345881+jundonghyuk@users.noreply.github.com>
2023-10-16 15:35:34 +0900

    [level 3] Title: 기지국 설치, Time: 0.75 ms, Memory: 53.7 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,39 +1,10 @@
 class Solution {
     public int solution(int n, int[] stations, int w) {
-        int cnt = 0;
-        int firstRest = stations[0] - w - 1;
-        if(firstRest > 0) {
-            int plus = firstRest / (2 * w + 1);
-            if(firstRest % (2 * w + 1) == 0) {
-                cnt += plus;  
-            } else {
-                cnt += plus + 1;
-            }
-        }
-        for(int i = 0; i < stations.length - 1; i++) {
-            int start = stations[i];
-            int end = stations[i+1];
-            
-            int rest = end - w  - 1 - (start + w);
-            if(rest <= 0) {
-                continue;
-            }
-            int plus = rest / (2 * w + 1);
-            if(rest % (2 * w + 1) == 0) {
-                cnt += plus;  
-            } else {
-                cnt += plus + 1;
-            }
-        }
-        int lastRest = n - (stations[stations.length - 1] + w);
-        if(lastRest > 0) {
-            int plus = lastRest / (2 * w + 1);
-            if(lastRest % (2 * w + 1) == 0) {
-                cnt += plus;  
-            } else {
-                cnt += plus + 1;
-            }
-        }
-        return cnt;
+        int answer = 0;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
     }
 }
