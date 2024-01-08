c8c99a91a8753dc0e269b6f90623d79cf80472a7
JunDonghyuk <84345881+jundonghyuk@users.noreply.github.com>
2024-01-08 16:48:29 +0900

    [Silver II] Title: N과 M (10), Time: 204 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,44 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    static int[] arr;
+    static int[] ret = new int[10];
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        nm(0, 0);
+        System.out.println(sb);
+    }
+
+    public static void nm(int depth, int start) {
+        if(depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(ret[i]);
+                sb.append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int temp = -1;
+        for (int i = start; i < N; i++) {
+            if(temp == arr[i]) {
+                continue;
+            }
+            temp = arr[i];
+            ret[depth] = arr[i];
+            nm(depth+1, i+1);
+        }
+    }
+}
