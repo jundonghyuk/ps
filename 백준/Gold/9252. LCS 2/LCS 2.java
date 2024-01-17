4689404f10828c6e59d167b00372e9e2cc72beea
JunDonghyuk <84345881+jundonghyuk@users.noreply.github.com>
2024-01-17 18:39:54 +0900

    [Gold IV] Title: LCS 2, Time: 248 ms, Memory: 21752 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -1,62 +1,55 @@
+
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 import java.util.Scanner;
 
+/*
+        6:02 ~
+        dp[i][j] : 1 ~ i, 1 ~ j 까지 최장 공통 수열
+        dp[i][j] = dp[i-1][j-1] + (일치), dp[i-1][j], dp[i][j-1]
+ */
 public class Main {
-    static int[][] storage;
-    static Deque<Character> dq = new LinkedList<>();
-    static int MAX_NUM = -1;
+
+    static int[][] dp;
+    static char[] first, second;
+    static Deque<Character> dq = new ArrayDeque<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String first = sc.nextLine();
-        String second = sc.nextLine();
-        int N = first.length();
-        int M = second.length();
-        storage = new int[N + 1][M + 1];
+        first = sc.nextLine().toCharArray();
+        second = sc.nextLine().toCharArray();
+        dp = new int[first.length + 1][second.length + 1];
 
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= M; j++) {
-
-                if (first.charAt(i - 1) == second.charAt(j - 1)) {
-                    storage[i][j] = storage[i - 1][j - 1] + 1;
-                } else {
-                    storage[i][j] = Math.max(storage[i - 1][j], storage[i][j - 1]);
-                }
-
-                MAX_NUM = Math.max(MAX_NUM, storage[i][j]);
+        for (int i = 1; i <= first.length; i++) {
+            for (int j = 1; j <= second.length; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + (first[i - 1] == second[j - 1] ? 1 : 0);
+                dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
+                dp[i][j] = Math.max(dp[i][j], dp[i][j - 1]);
             }
         }
 
-        for (int i = N; i >= 1; i--) {
-            for(int j = M; j >=1; j--) {
-                if(storage[i][j] == MAX_NUM) {
-                    find(i, j, first, second);
-                    System.out.println(MAX_NUM);
-                    String s = "";
-                    while(!dq.isEmpty()) {
-                        s = s + dq.pollFirst();
-                    }
-                    System.out.println(s);
-                    return;
-                }
-            }
-        }
-    }
+        int r = first.length;
+        int c = second.length;
 
-    static void find(int r, int c, String first, String second) {
-        if (r == 0 || c == 0) {
-            return;
-        }
-        if(first.charAt(r-1) == second.charAt(c-1)) {
-            dq.addFirst(first.charAt(r-1));
-            find(r-1,c-1,first,second);
-        } else {
-            if(storage[r][c-1] > storage[r-1][c]) {
-                find(r, c-1, first, second);
+        while (r > 0 && c > 0) {
+            if (first[r - 1] == second[c - 1]) {
+                dq.addFirst(first[r - 1]);
+                r--;
+                c--;
             } else {
-                find(r-1,c,first,second);
+                if(dp[r-1][c] > dp[r][c-1]) {
+                    r--;
+                } else {
+                    c--;
+                }
             }
         }
+
+        System.out.println(dp[first.length][second.length]);
+        StringBuilder sb = new StringBuilder();
+        for (char ch : dq) {
+            sb.append(ch);
+        }
+        System.out.println(sb);
     }
 }
