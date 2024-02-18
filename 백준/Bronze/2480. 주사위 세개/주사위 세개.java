73e1269e5202a1ace0cea5c9582faf42ebff12d2
JunDonghyuk <84345881+jundonghyuk@users.noreply.github.com>
2024-02-18 15:55:41 +0900

    [Bronze IV] Title: 주사위 세개, Time: 204 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        
+       	 	int num1 = sc.nextInt();
+        	int num2 = sc.nextInt();
+        	int num3 = sc.nextInt();
+        
+        	int result = 0;
+        
+        	if (num1 == num2 && num2 == num3 && num1 == num3) {
+        		result = 10000 + num1 * 1000;
+        	}
+        	else if (num1 == num2 || num1 == num3) {
+        		result = 1000 + num1 * 100;
+        	}
+        	else if (num2 == num3) {			// 이 부분을 빼먹고 풀어서 계속 틀렸었다.
+        		result = 1000 + num2 * 100;
+        	}
+        	else {
+        		result = Math.max(num1,  Math.max(num2, num3)) * 100;
+        	}
+        
+        	System.out.println(result);
+    }
+}
