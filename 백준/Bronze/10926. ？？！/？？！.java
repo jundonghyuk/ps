d363b178bd754cef8c3b8ac8c354cb605a08c946
JunDonghyuk <84345881+jundonghyuk@users.noreply.github.com>
2024-02-22 18:16:57 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+ 
+public class Main {
+    
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);	
+		String postFix = "??!";
+		System.out.println(sc.next() + postFix);
+	}
+}
