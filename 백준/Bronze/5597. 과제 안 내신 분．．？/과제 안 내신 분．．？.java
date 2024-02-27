e9e7e64bc72ad8d6ca0eec67fad292b372e2fbd5
JunDonghyuk <84345881+jundonghyuk@users.noreply.github.com>
2024-02-27 12:55:00 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 204 ms, Memory: 17500 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] students = new int[31];
+		
+		for (int i = 1; i < 29; i++) {
+			int success = sc.nextInt();
+			students[success] = 1;
+		}
+		for (int i = 1; i < students.length; i++) {
+			if (students[i] != 1)
+				System.out.println(i);
+		}
+	}
+}
