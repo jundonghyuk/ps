6d9226f6942d9ef12ce45f93804d8b5c5907330c
JunDonghyuk <84345881+jundonghyuk@users.noreply.github.com>
2024-03-01 14:03:30 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 172 ms, Memory: 14832 KB -BaekjoonHub

---
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        Deque<int[]> list = new ArrayDeque<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++){
+            int[] arr= {i + 1, Integer.parseInt(st.nextToken())};
+            list.add(arr);
+        }
+        while (list.size() > 1){
+            int[] arr= list.pollFirst();
+            sb.append(arr[0]).append(" ");
+            
+            int n = arr[1];
+            if(n>0) {
+                for(int j=1; j<n;j++) {
+                    list.offerLast(list.pollFirst());
+                }
+            }
+            else if(n<0){
+                for(int j=n;j!=0;j++) {
+                    list.offerFirst(list.pollLast());
+                }
+            }
+        }
+        sb.append(list.poll()[0]);
+        System.out.println(sb);
+    }
+}
